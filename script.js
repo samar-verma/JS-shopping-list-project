2b71208dc69a58b68a06db29da056ab75fc200bd
samar verma <samarverma004@gmail.com>
2024-06-23 00:17:28 +0530

    minor changes

--- script.js
+++ script.js
@@ -2,27 +2,35 @@ const form = document.getElementById('form');
 const itemInput = document.getElementById('item-input');
 const button = document.getElementById('btn');
 const list = document.getElementById('item-list');
+const liList = document.querySelectorAll('list');
 const clear = document.getElementById('clear-btn');
 const filter = document.getElementById('filter');
-button.addEventListener('click', (e) => {
+
+
+// adding item to list function
+function addItem(e) {
     e.preventDefault();
     item = itemInput.value;
-    
     if (item === '') {
         alert('please fill item');
-    } else {
+    } 
+     else {
         const li = document.createElement('li');
-        li.className = 'item';
-        li.innerText = item;
+        li.className ='item';
+        li.innerText =item;
         list.appendChild(li);
 
 
     }
-    itemInput.value = '';
-    return li;
+    itemInput.value ='';
+    // return li;
 
-});
+};
 
+
+
+
+// filter item function
 function filterItems(e) {
     const items = list.querySelectorAll('li');
     const text = e.target.value.toLowerCase();
@@ -32,6 +40,8 @@ function filterItems(e) {
   
       if (itemName.indexOf(text) != -1) {
         item.style.display = 'flex';
+      } else if (list.forEach) {
+
       } else {
         item.style.display = 'none';
       }
@@ -39,6 +49,7 @@ function filterItems(e) {
   };
 
 
+  // clear all function
   function clearItems() {
     while (list.firstChild) {
       list.removeChild(list.firstChild);
@@ -49,6 +60,6 @@ function filterItems(e) {
 
 filter.addEventListener('input', filterItems);
 clear.addEventListener('click', clearItems);
-
-
-
+button.addEventListener('click', addItem);
+console.log(list.querySelectorAll('li'));
+console.log(liList);
